e34dacee9dfb8c33f31db0b37d9ef1d86d8fab00
Ivan Shabanov <Ivan.shabanov@gmail.com>
2023-08-21 09:07:20 +0700

    chore: format/ arrays e.t.

--- class_sitemap.php
+++ class_sitemap.php
@@ -1,90 +1,102 @@
 <?php
-class sitemapXmlExt {
+class sitemapXmlExt
+{
 
-	var $ignore = array();
-	var $iBlocks = array();
-	var $pages = array();
-	var $sitemap = array();
+	public $ignore = [];
+	public  $iBlocks = [];
+	public  $pages = [];
+	public  $sitemap = [];
+	public  $HomeUrl = '/';
 
-	function __construct($home_url, $iBlocks = array(), $ignorePages = array()) {
+	function __construct($home_url, $iBlocks = [], $ignorePages = [])
+	{
 		$this->iBlocks = $iBlocks;
 		$this->ignore = $ignorePages;
 		$this->HomeUrl = $home_url;
 	}
 	/***************************************/
-	function generate() {
+	public function generate()
+	{
 		$this->AddPagesFromIBlock();
 		$this->pages = $this->DeleteIgnorePages();
 	}
 	/***************************************/
-	function array_merge($array, $arrayAdd) {
-		if (is_array($arrayAdd)) {
-			foreach ($arrayAdd as $val) {
-				$array[] = $val;
-			}
-		}
-		return $array;
-	}
-	/***************************************/
-	function DeleteIgnorePages()  {
+	public function DeleteIgnorePages()
+	{
 		$pages = $this->pages;
 		/*Не включаем игнорируемые страницы и дубли */
-		$result =  array();
-		$allready = array();
-		if (is_array($pages)) {
-			foreach ($pages as $val) {
-				if (!in_array($val['loc'], $this->ignore)) {
-					if (!in_array($val['loc'], $allready)) {
-						$allready[] = $val['loc'];
-						$result[] = $val;
-					}
-				}
+		$result =  [];
+		$allready = [];
+		if (!is_array($pages)) {
+			return $result;
+		};
+
+		foreach ($pages as $val) {
+			if (in_array($val['loc'], $this->ignore)) {
+				continue;
 			}
+			if (in_array($val['loc'], $allready)) {
+				continue;
+			}
+			$allready[] = trim($val['loc'], '/');
+			$allready[] = trim($val['loc'], '/') . '/';
+			$result[] = $val;
 		}
+
 		return $result;
 	}
 	/***************************************/
-	function AddPage($url, $priority = 1, $lastmod = '', $changefreq = 'monthly'){
+	public function AddPage($url, $priority = 1, $lastmod = '', $changefreq = 'monthly')
+	{
 		$url = str_replace('/index.php', '/', $url);
-		if (!in_array($url, $this->ignore)) {
-			$page = array();
-			$page['loc'] = $this->prepareLocation($url);
-			$page['priority'] = $priority;
-			if ($lastmod != '') {
-				$page['lastmod'] = date('Y-m-d', strtotime($lastmod));
-			} else {
-				$page['lastmod'] = date('Y-m-01');
-			}
-			if ($changefreq != '') {
-				$page['changefreq'] = $changefreq;
-			};
-			$this->pages[] = $page;
+		if (in_array($url, $this->ignore)) {
+			return false;
+		};
+
+		$page = [];
+		$page['loc'] = $this->prepareLocation($url);
+		$page['priority'] = $priority;
+		if ($lastmod != '') {
+			$page['lastmod'] = date('Y-m-d', strtotime($lastmod));
+		} else {
+			$page['lastmod'] = date('Y-m-01');
 		}
-	}/***************************************/
-	function AddIgnorePage($url){
+		if ($changefreq != '') {
+			$page['changefreq'] = $changefreq;
+		};
+		$this->pages[] = $page;
+
+		return $page;
+	}
+	/***************************************/
+	public function AddIgnorePage($url)
+	{
 		$this->ignore[] = $url;
 	}
 	/***************************************/
-	function CalcPriorBySlash($url) {
+	public function CalcPriorBySlash($url)
+	{
 		$url = str_replace($this->HomeUrl, '', $url);
-		$prior = 0.8 - (substr_count (trim($url, '/'), '/') * 0.1);
+		$prior = 0.8 - (substr_count(trim($url, '/'), '/') * 0.1);
 		if ($prior < 0.4) {
 			$prior = 0.4;
 		}
 		return $prior;
 	}
 	/***************************************/
-	function prepareLocation($url) {
+	public function prepareLocation($url)
+	{
 		if (mb_strpos($url, $this->HomeUrl) === false) {
 			if (mb_substr($url, 0, 1) != '/') {
-				$url = '/'.$url;
+				$url = '/' . $url;
 			};
 			$url = $this->HomeUrl . $url;
 		};
 		return $url;
 	}
 	/***************************************/
-	function AddPagesFromIBlock () {
+	public function AddPagesFromIBlock()
+	{
 		$arIBlocks = $this->iBlocks;
 		$arSectionIBlockFilter = [];
 		$arSectionIBlockIDs = [];
@@ -110,28 +122,28 @@ class sitemapXmlExt {
 
 			/* Вытащим все разделы */
 			if (count($arSectionIBlockIDs) > 0) {
-				$arSort = ["left_margin"=>"asc"];
+				$arSort = ["left_margin" => "asc"];
 				$arFilter = [
 					'IBLOCK_ID' => $arSectionIBlockIDs,
 					'ACTIVE' => 'Y'
 				];
 				$arSelect = [];
-				$res = CIBlockSection::GetList($arSort, $arFilter, true, $arSelect);
+				$res = \CIBlockSection::GetList($arSort, $arFilter, true, $arSelect);
 				while ($arSection = $res->GetNext()) {
-					$loc = $parentloc.$arSection['SECTION_PAGE_URL'];
+					$loc = $arSection['SECTION_PAGE_URL'];
 					$prior = $this->CalcPriorBySlash($loc);
 					$this->AddPage($loc, $prior, date('Y-m-01'), 'monthly');
 				}
 			}
 			if (count($arSectionIBlockFilter) > 0) {
 				foreach ($arSectionIBlockFilter as $IBlockID => $arFilter) {
-					$arSort = ["left_margin"=>"asc"];
+					$arSort = ["left_margin" => "asc"];
 					$arFilter['IBLOCK_ID'] = $IBlockID;
 					$arFilter['ACTIVE'] = 'Y';
 					$arSelect = ["*"];
-					$res = CIBlockSection::GetList($arSort, $arFilter, true, $arSelect);
+					$res = \CIBlockSection::GetList($arSort, $arFilter, true, $arSelect);
 					while ($arSection = $res->GetNext()) {
-						$loc = $parentloc.$arSection['SECTION_PAGE_URL'];
+						$loc = $arSection['SECTION_PAGE_URL'];
 						$prior = $this->CalcPriorBySlash($loc);
 						$this->AddPage($loc, $prior, date('Y-m-01'), 'monthly');
 					}
@@ -140,14 +152,14 @@ class sitemapXmlExt {
 			/* Вытащим все елементы */
 			$arSelect = ["*"];
 			$arFilter = [
-				"IBLOCK_ID"=>$arDetailIBlockIDs,
-				"ACTIVE_DATE"=>"Y",
-				"ACTIVE"=>"Y"
+				"IBLOCK_ID" => $arDetailIBlockIDs,
+				"ACTIVE_DATE" => "Y",
+				"ACTIVE" => "Y"
 			];
-			$res = CIBlockElement::GetList(array(), $arFilter, false, array("nPageSize"=>500000), $arSelect);
+			$res = \CIBlockElement::GetList([], $arFilter, false, array("nPageSize" => 500000), $arSelect);
 			while ($ob = $res->GetNextElement()) {
 				$arFields = $ob->GetFields();
-				$loc = $parentloc.$arFields['DETAIL_PAGE_URL'];
+				$loc = $arFields['DETAIL_PAGE_URL'];
 				$prior = $this->CalcPriorBySlash($loc);;
 				$this->AddPage($loc, $prior, date('Y-m-d', strtotime($arFields['TIMESTAMP_X'])), 'monthly');
 			}
@@ -158,7 +170,7 @@ class sitemapXmlExt {
 					$arFilter["IBLOCK_ID"] = $IBlockID;
 					$arFilter["ACTIVE_DATE"] = "Y";
 					$arFilter["ACTIVE"] = "Y";
-					$res = \CIBlockElement::GetList(array(), $arFilter, false, array("nPageSize" => 500000), $arSelect);
+					$res = \CIBlockElement::GetList([], $arFilter, false, array("nPageSize" => 500000), $arSelect);
 					while ($ob = $res->GetNextElement()) {
 						$arFields = $ob->GetFields();
 						$loc = $arFields['DETAIL_PAGE_URL'];
@@ -167,16 +179,16 @@ class sitemapXmlExt {
 					}
 				}
 			}
-
 		}
 	}
 	/******************************************************/
-	function AddPagesFromMenuFile($filemenu, $home_url) {
+	public function AddPagesFromMenuFile($filemenu, $home_url)
+	{
 		if (file_exists($filemenu)) {
 			@include($filemenu);
-			if (is_array($aMenuLinks )) {
+			if (is_array($aMenuLinks)) {
 				foreach ($aMenuLinks as $link) {
-					$loc = $home_url.$link[1];
+					$loc = $home_url . $link[1];
 					$prior = $this->CalcPriorBySlash($loc);
 					$this->AddPage($loc, $prior, date('Y-m-01'), 'monthly');
 				}
@@ -184,48 +196,50 @@ class sitemapXmlExt {
 		}
 	}
 	/******************************************************/
-	function Show($offset = 0, $length = 0) {
+	public function Show($offset = 0, $length = 0)
+	{
 		$pages = $this->pages;
 		if (($offset > 0) || ($length > 0)) {
 			$pages = array_slice($pages, $offset, $length);
 		};
-		if (is_array($pages)){
-			$result = '<?xml version="1.0" encoding="UTF-8"?>'."\n".
-						'<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
-			foreach ($pages as $key=>$val) {
+		$result = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+		$result .= '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . "\n";
+		if (is_array($pages)) {
+			foreach ($pages as $key => $val) {
 				if ($val['loc'] != '') {
 					$result .= '<url>';
-					foreach ($val as $valkey=>$valvalue) {
+					foreach ($val as $valkey => $valvalue) {
 						$valkey = htmlspecialchars($valkey, ENT_XML1);
 						$valvalue = htmlspecialchars($valvalue, ENT_XML1);
 						if ((trim($valvalue) != '') && ($valkey != 'title')) {
-							$result .= '<'.$valkey.'>'.$valvalue.'</'.$valkey.'>';
+							$result .= '<' . $valkey . '>' . $valvalue . '</' . $valkey . '>';
 						}
 					}
-					$result .= '</url>'."\n";
+					$result .= '</url>' . "\n";
 				}
 			}
-			$result .= '</urlset>';
 		}
+		$result .= '</urlset>';
 		return $result;
 	}
 
 	/*********************************** */
-	function AddToSitemapIndex($linkToSitemap) {
+	public function AddToSitemapIndex($linkToSitemap)
+	{
 		$this->sitemap[] = $linkToSitemap;
 	}
 
 	/*********************************** */
-	function ShowSitemapIndex() {
+	public function ShowSitemapIndex()
+	{
 		$sitemaps = $this->sitemap;
-		if (is_array($sitemaps)){
-			$result = '<?xml version="1.0" encoding="UTF-8"?>
-					<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-					';
+		$result = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+		$result .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+		if (is_array($sitemaps)) {
 			foreach ($sitemaps as $sitemap) {
 				$result .= '<sitemap>';
-				$result .= '<loc>'.$sitemap.'</loc>';
-				$result .= '<lastmod>'.date('Y-m-d').'</lastmod>';
+				$result .= '<loc>' . $sitemap . '</loc>';
+				$result .= '<lastmod>' . date('Y-m-d') . '</lastmod>';
 				$result .= '</sitemap>';
 			}
 		}
